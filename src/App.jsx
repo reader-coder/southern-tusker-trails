b17794180ea372e66671cca808ad1aa58ed06fdb
rahulr-codesvera <rahulrajmk@codesvera.com>
2025-03-28 13:54:25 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className='text-2xl'>App</h1>
+    </div>
+  )
+}
+
+export default App
