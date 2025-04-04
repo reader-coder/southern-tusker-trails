6843da1bf61e78b2af25666b6524d488608181d9
rahulr-codesvera <rahulrajmk@codesvera.com>
2025-04-04 18:15:14 +0530

    Replace timeline BG. Change bg-cover to bg-contain

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HomeBanner from '../components/HomeBanner/HomeBanner'
 import Timeline from '../components/Timeline/Timeline'
+import UpcomingPackages from '../components/UpcomingPackages/UpcomingPackages'
 
 const Home = () => {
   return (
     <>
     <HomeBanner/>
     <Timeline/>
+    <UpcomingPackages/>
     </>
   )
 }
