ba40be2f770e1647492c6988dadd085c8d2c78c6
rahulr-codesvera <rahulrajmk@codesvera.com>
2025-04-29 14:06:05 +0530

    add whatsapp button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet} from 'react-router'
 import Navbar from './components/Navbar/Navbar'
 import { ToastContainer } from 'react-toastify'
 import Footer from './components/Footer/Footer'
+import { FaWhatsapp } from 'react-icons/fa6'
 
 const App = () => {
   return (
@@ -11,6 +12,9 @@ const App = () => {
       <Outlet/>
       <Footer/>
       <ToastContainer />
+      <a href='https://wa.me/919633996929' target='_blank' rel="noopener noreferrer" className='fixed bottom-5 right-0 md:right-5 md:bottom-5 lg:right-10 lg:bottom-10 p-2 md:p-4 bg-[#075E54] rounded-tl-xl rounded-bl-xl md:rounded-full z-30 shadow-2xl'>
+        <FaWhatsapp className=' size-8 xl:size-12 text-white '/>
+      </a>
     </div>  )
 }
 
